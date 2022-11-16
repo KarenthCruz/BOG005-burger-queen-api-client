f310d50a7376a38299ed78a91641c0d176fc30f1
KarenthCruz <spoonecologic@gmail.com>
2022-11-16 09:32:17 -0500

    Working Test Login

---
+++ gourmet/test/loginView.spec.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { LoginView } from '../../src/components/loginView.jsx'
+import reactTest from 'react-test';
+import { render, screen } from 'react-test';
+import { user } from 'react-test/user-event';
+
+// describe('ValidationForm', () =>{
+//   const onSubmit = jest.fn();
+
+//   beforeEach(() =>{
+//     onSubmit.mockClear();x
+//     render(<ValidationForm onSubmit={onSubmit}/>);
+//   });
+
+//   it ('onSubmit is called when email input pass validation', () =>{
+//     const emailInput = screen.getByRole('textbox')
+//   })
+//   user.type(email, 'anita.borg@systers.xyz')
+// })
+
+
+
+
+// describe('login', () => {
+//   it('', () => {
+//     const view = LoginView();
+//     const loginForm = view.querySelector('.loginForm');
+//     loginForm.onSubmit();
+//     expect().toBeCalled();
+//   });
+// })
+
